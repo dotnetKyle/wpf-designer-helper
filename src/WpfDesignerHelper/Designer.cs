b7043e96b0cdc7dc051b5f27c8d10a8a04fc1fc3
Kyle <dotnetKyle@gmail.com>
2021-04-02 13:22:40 -0400

    Initial commit

---
+++ src/WpfDesignerHelper/Designer.cs
@@ -0,0 +1,37 @@
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+
+namespace WpfDesignerHelper
+{
+    internal static class Designer
+    {
+        public static bool Active
+        {
+            get
+            {
+                try
+                {
+                    return (bool)DesignerProperties.IsInDesignModeProperty.GetMetadata(typeof(DependencyObject)).DefaultValue;
+                }
+                catch(Exception ex)
+                {
+                    var errorMessage = $"{typeof(Designer).FullName} ran into a problem while trying to detect if the design was Active:\r\n" +
+                            $"    {ex}";
+
+                    if(Debugger.IsAttached)
+                    {
+                        Debug.WriteLine(errorMessage);
+                    }
+                    else
+                    {
+                        Console.WriteLine(errorMessage);
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+}
